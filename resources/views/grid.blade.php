87cca78981689fffbc3cb1801f4ce38a84f0c614
Sanjeepastha <sanjeepa.shrestha@olivemedia.co>
2017-07-27 17:42:46 +0545

    Initial setup with grid system using sass

---
+++ resources/views/grid.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+<body>
+    <div class="containerr">
+       <h2> Grids</h2>
+<!--
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        
+        <div class="col-xs-2">Grid</div>
+        <div class="col-xs-2">Grid</div>
+        <div class="col-xs-2">Grid</div>
+        <div class="col-xs-2">Grid</div>
+        <div class="col-xs-2">Grid</div>
+        <div class="col-xs-2">Grid</div>
+        
+        <div class="col-xs-3">Grid</div>
+        <div class="col-xs-3">Grid</div>
+        <div class="col-xs-3">Grid</div>
+        <div class="col-xs-3">Grid</div>
+        
+        <div class="col-xs-4">Grid</div>
+        <div class="col-xs-4">Grid</div>
+        <div class="col-xs-4">Grid</div>
+        
+        <div class="col-xs-5">Grid</div>
+        <div class="col-xs-7">Grid</div>
+        
+        <div class="col-xs-6">Grid</div>
+        <div class="col-xs-6">Grid</div>
+        
+        <div class="col-xs-8">Grid</div>
+        <div class="col-xs-4">Grid</div>
+        
+        <div class="col-xs-9">Grid</div>
+        <div class="col-xs-3">Grid</div>
+        
+         <div class="col-xs-10">Grid</div>
+        <div class="col-xs-2">Grid</div>
+        
+         <div class="col-xs-11">Grid</div>
+        <div class="col-xs-1">Grid</div>
+        
+        <div class="col-xs-12">Grid</div>
+-->
+  
+<!--
+       <div class="col-xs-1">col-1</div>
+        <div class="col-xs-2">col-2</div>
+       <div class="col-xs-3">col-3</div>
+       <div class="col-xs-4">col-4</div>
+       <div class="col-xs-5">col-5</div>
+       <div class="col-xs-6">col-6</div>
+       <div class="col-xs-7">col-7</div>
+       <div class="col-xs-8">col-8</div>
+       <div class="col-xs-9">col-9</div>
+       <div class="col-xs-10">col-10</div>
+       <div class="col-xs-11">col-11</div>
+       <div class="col-xs-12">col-12</div>
+       <div class="col-xs-13">col-13</div>
+       <div class="col-xs-14">col-14</div>
+       <div class="col-xs-15">col-15</div>
+       <div class="col-xs-16">col-16</div>
+       <div class="col-xs-17">col-17</div>
+       <div class="col-xs-18">col-18</div>
+       <div class="col-xs-19">col-19</div>
+       <div class="col-xs-20">col-20</div>
+    
+-->
+<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">grid 1</div>
+ <div class="col-lg-6 col-md-6 col-sm-4 col-xs-12">grid 2</div>       
+        
+    </div>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+<link href="{{asset('css/app.css')}}" rel="stylesheet">
+<link href="{{asset('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+</head>
+<body>
+     <div class="wrapper">
+       <h2>Navigation</h2>
+        <div class="container">
+           <nav class="first-nav">
+              <ul>
+                <li class="active"><a><i class="fa fa-home"></i> HOME</a></li>
+                <li><a><i class="fa fa-archive" aria-hidden="true"></i> ARCHIVE</a></li>
+                <li><a><i class="fa fa-pie-chart" aria-hidden="true"></i> ANALYTICS</a></li>
+                <li><a><i class="fa fa-upload" aria-hidden="true"></i> UPLOAD</a></li>
+                <li><a><i class="fa fa-cog"></i> SETTINGS</a></li>
+
+              </ul> 
+               
+           </nav>
+           <nav class="second-nav">
+              <ul>
+                <li class="active"><a><i class="fa fa-home"></i> HOME</a></li>
+                <li><a><i class="fa fa-archive" aria-hidden="true"></i> ARCHIVE</a></li>
+                <li><a><i class="fa fa-pie-chart" aria-hidden="true"></i> ANALYTICS</a></li>
+                <li><a><i class="fa fa-upload" aria-hidden="true"></i> UPLOAD</a></li>
+                <li><a><i class="fa fa-cog"></i> SETTINGS</a></li>
+
+              </ul> 
+               
+           </nav>
+            
+        </div> 
+         
+     </div>
+</body>
+</html>
